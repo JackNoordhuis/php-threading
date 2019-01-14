880d8527909b6f7c9e157c9b8d18ef448a8fe870
Jack Noordhuis <daswaggamer@gmail.com>
2019-01-14 17:11:20 +1100

    Initial commit

---
+++ src/Thread.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine;
+
+use jacknoordhuis\Autoload\ThreadedClassLoader;
+
+/**
+ * This class must be extended by all custom threading classes
+ */
+class Thread extends \Thread {
+
+	/** @var \jacknoordhuis\Autoload\ThreadedClassLoader */
+	protected $classLoader;
+
+	/** @var bool */
+	protected $isKilled = false;
+
+	/**
+	 * @return \jacknoordhuis\Autoload\ThreadedClassLoader
+	 */
+	public function getClassLoader() {
+		return $this->classLoader;
+	}
+
+	/**
+	 * @param \jacknoordhuis\Autoload\ThreadedClassLoader $classLoader
+	 */
+	public function setClassLoader(ThreadedClassLoader $classLoader) : void {
+		$this->classLoader = $classLoader;
+	}
+
+	/**
+	 * Registers the class loader for this thread.
+	 *
+	 * WARNING: This method MUST be called from any child threads' run() method to make auto-loading usable.
+	 * If you do not do this, you will not be able to use new classes that were not loaded when the thread was started
+	 * (unless you are using a custom autoloader).
+	 */
+	public function registerClassLoader() : void {
+		if($this->classLoader !== null) {
+			$this->classLoader->register(false);
+		}
+	}
+
+	public function start(?int $options = \PTHREADS_INHERIT_ALL) {
+		ThreadManager::getInstance()->add($this);
+
+		if(!$this->isRunning() and !$this->isJoined() and !$this->isTerminated()) {
+			return parent::start($options);
+		}
+
+		return false;
+	}
+
+	/**
+	 * Stops the thread using the best way possible. Try to stop it yourself before calling this.
+	 */
+	public function quit() : void {
+		$this->isKilled = true;
+
+		$this->notify();
+
+		if(!$this->isJoined()) {
+			if(!$this->isTerminated()) {
+				$this->join();
+			}
+		}
+
+		ThreadManager::getInstance()->remove($this);
+	}
+
+	public function getThreadName() : string {
+		return (new \ReflectionClass($this))->getShortName();
+	}
+
+}
---
+++ src/ThreadManager.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine;
+
+use Generator;
+use Logger;
+
+/**
+ * Class that keeps track of all the threads
+ */
+class ThreadManager {
+
+	/** @var ThreadManager */
+	private static $instance = null;
+
+	public static function init(Logger $logger) : void {
+		self::$instance = new ThreadManager();
+	}
+
+	/**
+	 * @return ThreadManager
+	 */
+	public static function getInstance() : ThreadManager {
+		return self::$instance;
+	}
+
+	/**
+	 * @param Worker|Thread $thread
+	 */
+	public function add($thread) : void {
+		if($thread instanceof Thread or $thread instanceof Worker) {
+			$this->{spl_object_hash($thread)} = $thread;
+		}
+	}
+
+	/**
+	 * @param Worker|Thread $thread
+	 */
+	public function remove($thread) : void {
+		if($thread instanceof Thread or $thread instanceof Worker) {
+			unset($this->{spl_object_hash($thread)});
+		}
+	}
+
+	/**
+	 * @return Worker[]|Thread[]
+	 */
+	public function getAll() : array {
+		$array = [];
+		foreach($this as $key => $thread) {
+			$array[$key] = $thread;
+		}
+
+		return $array;
+	}
+
+	/**
+	 * @param int $erroredThreads
+	 *
+	 * @return \Generator
+	 */
+	public function stopAll(int &$erroredThreads = 0) : Generator {
+		foreach($this->getAll() as $thread) {
+			yield "Stopping " . $thread->getThreadName() . " thread";
+			try {
+				$thread->quit();
+				yield $thread->getThreadName() . " thread stopped successfully.";
+			} catch(\ThreadException $e) {
+				++$erroredThreads;
+				yield "Could not stop " . $thread->getThreadName() . " thread: " . $e->getMessage();
+			}
+		}
+	}
+
+}
